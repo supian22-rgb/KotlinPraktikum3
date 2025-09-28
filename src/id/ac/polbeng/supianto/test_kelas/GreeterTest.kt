120d885173ae7c5ef089e8fc93e041b84c9c1894
iyan <supian22@gmail.com>
2025-09-28 07:29:06 +0700

    praktikum3

--- src/id/ac/polbeng/supianto/test_kelas/GreeterTest.kt
+++ src/id/ac/polbeng/supianto/test_kelas/GreeterTest.kt
@@ -2,4 +2,9 @@ package id.ac.polbeng.supianto.test_kelas
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
 }
